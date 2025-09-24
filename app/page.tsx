a673b099cfca11f5234fe4dde3ea99145696bc87
tarou-samurai <tarou-samurai@gmail.com>
2025-09-24 11:56:34 +0900

    vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
 export default function Home() {
   // Server-side redirect to the login page
-  redirect('/login')
-  return null
+  redirect("/login");
+  return null;
 }
